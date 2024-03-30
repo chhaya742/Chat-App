9e621e5d47ace1b32343cb73b2701a2e4cb31324
Chhaya Bagwan <chhaya20@navgurukul.org>
2024-03-30 11:55:02 +0530

    changes

--- backend/DB/connectToMongoDB.js
+++ backend/DB/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
+		await mongoose.connect("mongodb+srv://chhaya20:A56jAiJnMvcQ1RwL@cluster0.qs900tc.mongodb.net/chat-app-db?retryWrites=true&w=majority&appName=Cluster0");
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
