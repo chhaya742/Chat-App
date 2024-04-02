6f92a683d2f45bfafc23278f44c25260d09cbe59
chhaya bagwan <76725368+chhaya742@users.noreply.github.com>
2024-04-02 15:42:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home/Home";
-// import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/SignUp";
+import Home from "./pages/home/Home.jsx";
+import Login from "./pages/login/Login.jsx";
+import SignUp from "./pages/signup/SignUp.jsx";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
 
@@ -12,7 +12,7 @@ function App() {
 		<div className='p-4 h-screen flex items-center justify-center'>
 			<Routes>
 				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
-{/* 				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} /> */}
+				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
 				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
 			</Routes>
 			<Toaster />
